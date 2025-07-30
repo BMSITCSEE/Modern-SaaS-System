5e79808e5e4a00d134e0857ae147a4378d05ca66
BMSITCSEE <156016070+BMSITCSEE@users.noreply.github.com>
2025-07-30 21:59:24 +0530

    Update ThemeProvider.tsx

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -3,7 +3,8 @@
 import * as React from "react"
 import dynamic from "next/dynamic"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import type { ThemeProviderProps } from "next-themes"
+
 
 function ThemeProviderComponent({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
